a5fdf4e6e655c4cec90b2bb9cd2f766848273f8e
NicoAble <113350322+NicoAble@users.noreply.github.com>
2022-10-24 13:19:27 +0200

    Update 100NumPrimi.py
    correction

--- Python/100NumPrimi.py
+++ Python/100NumPrimi.py
@@ -1,8 +1,8 @@
-def controllo(a, b):
+def controllo(a):
+    b=True
     for i in range(2, int(a**0.5)+1):
         if a%i==0: b=False
     return b
 
-c=True
-l=[i*(if controllo(i, c)==True) for i in range(1, 100+1)]
+l=[i for i in range(1, 100+1) if controllo(i)]
 print(l)
